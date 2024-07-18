2a8c10a6944e923db415c0e3b37f3ea53a2d3690
Pamela Dantas <pameladantasp@gmail.com>
2024-07-17 21:53:28 -0300

    [sections][js] adicionar sections da pagina e functions do js
    Seções: Sobre a cooperativa - Login - Buscar produtos
    
    Funções:
    ao clicar no card, a pagina recarrega e vai para a seção clicada.
        também oculta o banner ao ir para uma seção.
    ao clicar no logotipo na header, a pagina volta para a pagina principal
    com os 3 cards.

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,52 @@ window.addEventListener('scroll', function() {
         logoContainer.classList.remove('scrolled');
     }
 });
+
+// clicar na logo volta pra página inicial
+function main() {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        section.style.display = 'none'; // Esconde todas as seções
+    });
+
+    document.querySelector('header').style.display = 'flex'; // banner
+    document.getElementById('cards-container').style.display = 'flex'; // 3 cards 
+}
+
+
+
+// abrir seção por seção na mesma página, sem abrir outra
+function hideAllSections() {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+}
+
+function sectionSobre() {
+    hideAllSections();
+    document.getElementById('sobre-cooperativa').style.display = 'block';
+    document.getElementById('cards-container').style.display = 'none';
+    document.getElementById('banner').style.display = 'none';
+}
+
+function sectionLogin() {
+    hideAllSections();
+    document.getElementById('login').style.display = 'block';
+    document.getElementById('cards-container').style.display = 'none';
+    document.getElementById('banner').style.display = 'none';
+}
+
+function sectionBuscar() {
+    hideAllSections();
+    document.getElementById('buscar-produtos').style.display = 'block';
+    document.getElementById('cards-container').style.display = 'none';
+    document.getElementById('banner').style.display = 'none';
+}
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Adicione um ouvinte de evento para o carregamento do DOM
+// });
+
+
+
